844d26c7a74b3a2cb52010b2e65408887b2613c5
liwenlong <liwenlong@aikaka.cc>
2016-08-17 17:40:39 +0800

    调整page目录结构

--- my_note/page/app/controller/demo.js
+++ my_note/page/app/controller/demo.js
@@ -25,19 +25,19 @@ app.run(function($transform) {
 // in order to avoid unwanted routing.
 // 
 app.config(function($routeProvider) {
-  $routeProvider.when('/',              {templateUrl: 'home.html', reloadOnSearch: false});
-  $routeProvider.when('/note',          {templateUrl: 'note.html', reloadOnSearch: false}); 
-  $routeProvider.when('/scroll',        {templateUrl: 'scroll.html', reloadOnSearch: false}); 
-  $routeProvider.when('/toggle',        {templateUrl: 'toggle.html', reloadOnSearch: false}); 
-  $routeProvider.when('/accordion',     {templateUrl: 'accordion.html', reloadOnSearch: false}); 
-  $routeProvider.when('/overlay',       {templateUrl: 'overlay.html', reloadOnSearch: false}); 
-  $routeProvider.when('/forms',         {templateUrl: 'forms.html', reloadOnSearch: false});
-  $routeProvider.when('/dropdown',      {templateUrl: 'dropdown.html', reloadOnSearch: false});
-  $routeProvider.when('/touch',         {templateUrl: 'touch.html', reloadOnSearch: false});
-  $routeProvider.when('/swipe',         {templateUrl: 'swipe.html', reloadOnSearch: false});
-  $routeProvider.when('/drag',          {templateUrl: 'drag.html', reloadOnSearch: false});
-  $routeProvider.when('/drag2',         {templateUrl: 'drag2.html', reloadOnSearch: false});
-  $routeProvider.when('/carousel',      {templateUrl: 'carousel.html', reloadOnSearch: false});
+  $routeProvider.when('/',              {templateUrl: 'app/view/home.html', reloadOnSearch: false});
+  $routeProvider.when('/note',          {templateUrl: 'app/view/note.html', reloadOnSearch: false}); 
+  $routeProvider.when('/scroll',        {templateUrl: 'app/view/scroll.html', reloadOnSearch: false}); 
+  $routeProvider.when('/toggle',        {templateUrl: 'app/view/toggle.html', reloadOnSearch: false}); 
+  $routeProvider.when('/accordion',     {templateUrl: 'app/view/accordion.html', reloadOnSearch: false}); 
+  $routeProvider.when('/overlay',       {templateUrl: 'app/view/overlay.html', reloadOnSearch: false}); 
+  $routeProvider.when('/forms',         {templateUrl: 'app/view/forms.html', reloadOnSearch: false});
+  $routeProvider.when('/dropdown',      {templateUrl: 'app/view/dropdown.html', reloadOnSearch: false});
+  $routeProvider.when('/touch',         {templateUrl: 'app/view/touch.html', reloadOnSearch: false});
+  $routeProvider.when('/swipe',         {templateUrl: 'app/view/swipe.html', reloadOnSearch: false});
+  $routeProvider.when('/drag',          {templateUrl: 'app/view/drag.html', reloadOnSearch: false});
+  $routeProvider.when('/drag2',         {templateUrl: 'app/view/drag2.html', reloadOnSearch: false});
+  $routeProvider.when('/carousel',      {templateUrl: 'app/view/carousel.html', reloadOnSearch: false});
 });
 
 // 
