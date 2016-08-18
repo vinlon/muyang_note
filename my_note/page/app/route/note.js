9c94b6e212ced01d38de0c370552c54ace0d6cad
liwenlong <liwenlong@aikaka.cc>
2016-08-18 11:33:19 +0800

    添加帮助页面（空）

--- my_note/page/app/route/note.js
+++ my_note/page/app/route/note.js
@@ -7,13 +7,13 @@ app.config(function($routeProvider) {
   $routeProvider.when('/',              {templateUrl: 'app/view/home.html', reloadOnSearch: false});
   $routeProvider.when('/note',          {templateUrl: 'app/view/note.html', reloadOnSearch: false}); 
   $routeProvider.when('/carousel',      {templateUrl: 'app/view/carousel.html', reloadOnSearch: false});
+  $routeProvider.when('/help',      {templateUrl: 'app/view/help.html', reloadOnSearch: false});
   
   $routeProvider.when('/scroll',        {templateUrl: 'app/view/scroll.html', reloadOnSearch: false}); 
   $routeProvider.when('/toggle',        {templateUrl: 'app/view/toggle.html', reloadOnSearch: false}); 
   $routeProvider.when('/accordion',     {templateUrl: 'app/view/accordion.html', reloadOnSearch: false}); 
   $routeProvider.when('/overlay',       {templateUrl: 'app/view/overlay.html', reloadOnSearch: false}); 
   $routeProvider.when('/forms',         {templateUrl: 'app/view/forms.html', reloadOnSearch: false});
-  $routeProvider.when('/dropdown',      {templateUrl: 'app/view/dropdown.html', reloadOnSearch: false});
   $routeProvider.when('/touch',         {templateUrl: 'app/view/touch.html', reloadOnSearch: false});
   $routeProvider.when('/swipe',         {templateUrl: 'app/view/swipe.html', reloadOnSearch: false});
   $routeProvider.when('/drag',          {templateUrl: 'app/view/drag.html', reloadOnSearch: false});
